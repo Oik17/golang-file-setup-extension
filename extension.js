5de2c2dcd27f15e2fd1ecb5c94d7a6323e34c21f
Akshat Gupta <akshat.g1707@gmail.com>
2024-08-16 14:06:48 +0530

    feat: echo/gin setup main.go

--- extension.js
+++ extension.js
@@ -1,36 +1,89 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const fs = require('fs');
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    // Hello World Command
+    let helloWorldCommand = vscode.commands.registerCommand('golang-sqlx-file-setup.helloWorld', function () {
+        vscode.window.showInformationMessage('Hello World from golang-sqlx-file-setup!');
+    });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "golang-sqlx-file-setup" is now active!');
+    // Setup Project Command
+    let setupProjectCommand = vscode.commands.registerCommand('golang-sqlx-file-setup.setupProject', async function () {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (workspaceFolders) {
+            const projectPath = workspaceFolders[0].uri.fsPath;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('golang-sqlx-file-setup.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+            // Prompt user for framework choice
+            const framework = await vscode.window.showInputBox({
+                prompt: 'Enter the framework (gin/echo)',
+                placeHolder: 'gin or echo'
+            });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from golang-sqlx-file-setup!');
-	});
+            let mainGoContent;
+            switch (framework) {
+                case 'gin':
+                    mainGoContent = `
+package main
 
-	context.subscriptions.push(disposable);
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello, Gin!",
+	})
+	})
+	r.Run(":8080")
+}
+                    `;
+                    break;
+                case 'echo':
+                    mainGoContent = `
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context){
+	return c.JSON(http.StatusOK, map[string]string{"message": "Hello, Echo!"})
+	})
+	e.Start(":8080")
+}
+                    `;
+                    break;
+                default:
+                    vscode.window.showErrorMessage('Unsupported framework. Please enter "gin" or "echo".');
+                    return;
+            }
+
+            // Create the main.go file
+            const mainGoPath = path.join(projectPath, 'main.go');
+            fs.writeFileSync(mainGoPath, mainGoContent);
+
+            vscode.window.showInformationMessage(`Golang project setup with ${framework} framework completed!`);
+        } else {
+            vscode.window.showErrorMessage('No workspace folder found!');
+        }
+    });
+
+    context.subscriptions.push(helloWorldCommand);
+    context.subscriptions.push(setupProjectCommand);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
